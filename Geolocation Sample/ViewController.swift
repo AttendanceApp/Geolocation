cf7084c7667794068bbbec92f08f3813c29fdc93
Cantevisa <18vwoo@castilleja.org>
2017-02-03 08:50:24 -0800

    Initial Commit

---
+++ Geolocation Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geolocation Sample
+//
+//  Created by Vanessa Woo on 2/3/17.
+//  Copyright © 2017 Attendance App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
