97294a2b57ee1a419230d386a22880e8a2616025
Cantevisa <18vwoo@castilleja.org>
2017-02-09 08:19:04 -0800

    Geolocation Sample created!

--- Geolocation Sample/ViewController.swift
+++ Geolocation Sample/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var timer: Timer?
+    
+    struct MapConstants {
+        static let latitude = 37.438696
+        static let longitude = -122.152208
+    }
+    
+    let geofence: Geofence = Geofence(latitudeDeadband: 0.5, longitudeDeadband: 0.5, targetLatitude: MapConstants.latitude, targetLongitude: MapConstants.longitude)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateLocationLabel), userInfo: nil, repeats: true)
+    }
+    
+    func updateLocationLabel() {
+        if (geofence.inCasti) {
+            label.text = "You are on the Casti campus."
+        } else {
+            label.text = "You are not on the Casti campus."
+        }
+        print(geofence.inCasti)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    deinit {
+        if let timer = self.timer {
+            timer.invalidate()
+        }
+    }
 
 }
 
